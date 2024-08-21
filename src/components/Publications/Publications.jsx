74de6c105c570170587fa6171f7c0552169ef287
Abhinav mishra <abhinavm945@gmail.com>
2024-08-21 15:54:51 +0530

    message to commit

--- src/components/Publications/Publications.jsx
+++ src/components/Publications/Publications.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function Publications() {
   return (
-    <div className='h-screen w-full pl-40 pr-40'>
-        <br/>
-        Publications of arun kumar verma mkmmmmmmkkmkkmkkkmkkmkm
-      </div>
-  )
+    <div className="h-screen w-full pl-40 pr-40">
+      <br />
+      Publications of arun
+    </div>
+  );
 }
 
-export default Publications
+export default Publications;
