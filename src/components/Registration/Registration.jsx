469af0b75592f128a335ca36b7e05545ba82c62b
Raja Abrar Khan <abrarkhan778008@gmail.com>
2024-08-23 20:01:34 +0530

    commit newww

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 function Registration() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
